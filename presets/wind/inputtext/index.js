0d478d52220d4fe5fbe26b46673ab2b34057cb52
Toprak Koç <hello@toprak.dev>
2024-02-13 16:00:09 +0300

    Updated InputText themes to allow PT classes

--- presets/wind/inputtext/index.js
+++ presets/wind/inputtext/index.js
@@ -1,39 +1,48 @@
 import { classNames } from 'primereact/utils';
 
 export default {
-    root: ({ props, context, parent }) => ({
-        className: classNames(
-            props.className,
-            // Font
-            'font-sans leading-6',
+    root: ({
+        props,
+        context,
+        parent,
+        props: {
+            pt: { root }
+        }
+    }) => ({
+        className:
+            root.className ||
+            classNames(
+                props.className,
+                // Font
+                'font-sans leading-6',
 
-            // Spacing
-            'm-0',
-            {
-                'py-3 px-4 text-lg sm:text-md': props.size == 'large',
-                'py-1 px-2 sm:text-sm': props.size == 'small',
-                'py-1.5 px-3 sm:text-sm': props.size == null
-            },
+                // Spacing
+                'm-0',
+                {
+                    'py-3 px-4 text-lg sm:text-md': props.size == 'large',
+                    'py-1 px-2 sm:text-sm': props.size == 'small',
+                    'py-1.5 px-3 sm:text-sm': props.size == null
+                },
 
-            // Colors
-            'text-surface-900 dark:text-surface-0',
-            'placeholder:text-surface-400 dark:placeholder:text-surface-500',
-            'bg-surface-0 dark:bg-surface-900',
-            'shadow-sm',
-            { 'ring-1 ring-inset ring-surface-300 dark:ring-surface-700 ring-offset-0': parent?.state['__TYPE'] !== 'InputGroup' },
+                // Colors
+                'text-surface-900 dark:text-surface-0',
+                'placeholder:text-surface-400 dark:placeholder:text-surface-500',
+                'bg-surface-0 dark:bg-surface-900',
+                'shadow-sm',
+                { 'ring-1 ring-inset ring-surface-300 dark:ring-surface-700 ring-offset-0': parent?.state['__TYPE'] !== 'InputGroup' },
 
-            // Shape
-            { 'rounded-md': parent?.state['__TYPE'] !== 'InputGroup' },
-            { 'first:rounded-l-md rounded-none last:rounded-r-md': parent?.state['__TYPE'] == 'InputGroup' },
-            { 'border-0 border-y border-l last:border-r border-surface-300 dark:border-surface-600': parent?.state['__TYPE'] == 'InputGroup' },
-            { 'first:ml-0 ml-[-1px]': parent?.state['__TYPE'] == 'InputGroup' && !props.showButtons },
-            'appearance-none',
+                // Shape
+                { 'rounded-md': parent?.state['__TYPE'] !== 'InputGroup' },
+                { 'first:rounded-l-md rounded-none last:rounded-r-md': parent?.state['__TYPE'] == 'InputGroup' },
+                { 'border-0 border-y border-l last:border-r border-surface-300 dark:border-surface-600': parent?.state['__TYPE'] == 'InputGroup' },
+                { 'first:ml-0 ml-[-1px]': parent?.state['__TYPE'] == 'InputGroup' && !props.showButtons },
+                'appearance-none',
 
-            // Interactions
-            {
-                'outline-none focus:ring-primary-500 dark:focus:ring-primary-400': !context.disabled,
-                'opacity-60 select-none pointer-events-none cursor-default': context.disabled
-            }
-        )
+                // Interactions
+                {
+                    'outline-none focus:ring-primary-500 dark:focus:ring-primary-400': !context.disabled,
+                    'opacity-60 select-none pointer-events-none cursor-default': context.disabled
+                }
+            )
     })
 };
